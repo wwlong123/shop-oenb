90d8cff1906793c49d7ad768ae2422f6dabe9e6b
wwl <460244011@qq.com>
2018-10-30 16:16:13 +0800

    wwl

---
+++ src/main/java/com/jk/ShopOenbApplication.java
@@ -0,0 +1,12 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopOenbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopOenbApplication.class, args);
+    }
+}
